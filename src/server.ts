2c79acb1444c553e8875c8ffb96852b40b832035
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2023-12-23 18:01:43 -0300

    rota de uploads de imagens e videos

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
-import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
 app.register(jwt, {
   secret: 'spacetime'
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
